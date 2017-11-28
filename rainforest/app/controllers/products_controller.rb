68afe9223d009a85d12ca0ee20de50e94994c0c1
co2co2 <coco8287@hotmail.com>
2017-11-28 11:01:39 -0500

    add methods in controller

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -2,18 +2,32 @@ class ProductsController < ApplicationController
 
 
   def index
+    @products = Product.all
   end
 
   def create
+    @product = Product.new
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_url
+    else
+      redirect_to new_product_url
+    end
   end
 
+
   def new
+    @product = Product.new
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def update

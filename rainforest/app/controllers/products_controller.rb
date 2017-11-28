e8d3c9622c28e9e13a63675d670d11b9d3aa9586
co2co2 <coco8287@hotmail.com>
2017-11-28 12:00:04 -0500

    controller updated

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -31,9 +31,23 @@ class ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_url
+    else
+      redirect_to new_product_url
+    end
   end
 
+
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
   end
 
 

a4e24d09b239f6ec966b7d33594c864d2ce23beb
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-28 11:16:39 -0500

    added test data

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.create(name: 'Computer', description: 'Device to access the web', price_in_cents: 150000)
+
+product2 = Product.create(name: 'Television', description: 'Watch TV shows', price_in_cents: 30000)
+#
+#
+# t.string "name"
+# t.text "description"
+# t.integer "price_in_cents"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false

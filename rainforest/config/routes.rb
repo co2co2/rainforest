52f811ab9386307dec0e61d27993784624e14d7b
co2co2 <coco8287@hotmail.com>
2017-11-29 19:01:05 -0500

    user authenticate done

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+  resource :users, only: %i(create new)
+  resource :sessions,only: %i(create new destroy)
+
   root 'products#index'
 
   resources :products do
